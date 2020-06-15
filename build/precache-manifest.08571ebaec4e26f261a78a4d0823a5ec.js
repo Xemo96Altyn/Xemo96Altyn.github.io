a4561c111d9579c61e8f664d52328b2f9a75374f
Xemo96Altyn <64924071+Xemo96Altyn@users.noreply.github.com>
2020-06-15 20:53:14 +0300

    Add files via upload

---
+++ build/precache-manifest.08571ebaec4e26f261a78a4d0823a5ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e1527c40caa110083e42f941a32b46f",
+    "url": "/vkapp/index.html"
+  },
+  {
+    "revision": "a0b8e10c19a48db6465f",
+    "url": "/vkapp/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "26fa136497c651b9bdd5",
+    "url": "/vkapp/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "a0b8e10c19a48db6465f",
+    "url": "/vkapp/static/js/2.85c0abf8.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/vkapp/static/js/2.85c0abf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26fa136497c651b9bdd5",
+    "url": "/vkapp/static/js/main.57072282.chunk.js"
+  },
+  {
+    "revision": "225b4b5c0656f59d4872",
+    "url": "/vkapp/static/js/runtime-main.270aa3ca.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vkapp/static/media/persik.4e1ec840.png"
+  }
+]);
