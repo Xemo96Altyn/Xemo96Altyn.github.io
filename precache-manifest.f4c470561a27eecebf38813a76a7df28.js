fc75ee60534983ed502cd77adedac45f20d60960
Xemo96Altyn <igoryasha444@gmail.com>
2020-06-15 20:13:41 +0300

    Updates

---
+++ precache-manifest.f4c470561a27eecebf38813a76a7df28.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52156dc4547641d4569f4e0ede0ca402",
+    "url": "/Xemo96Altyn.github.io/index.html"
+  },
+  {
+    "revision": "b1c60ecce44035f3ab06",
+    "url": "/Xemo96Altyn.github.io/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "a0867320a713dedc98c6",
+    "url": "/Xemo96Altyn.github.io/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "b1c60ecce44035f3ab06",
+    "url": "/Xemo96Altyn.github.io/static/js/2.e13e9fe5.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/Xemo96Altyn.github.io/static/js/2.e13e9fe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0867320a713dedc98c6",
+    "url": "/Xemo96Altyn.github.io/static/js/main.214da33b.chunk.js"
+  },
+  {
+    "revision": "4cea8e54d260b737d381",
+    "url": "/Xemo96Altyn.github.io/static/js/runtime-main.8fed81ab.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/Xemo96Altyn.github.io/static/media/persik.4e1ec840.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Xemo96Altyn.github.io/precache-manifest.f4c470561a27eecebf38813a76a7df28.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Xemo96Altyn.github.io/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
